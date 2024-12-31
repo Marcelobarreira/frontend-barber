11caf9a3f7d1dd5746c649d6f0a570cfacc1bea5
marcelodestcert <marcelo@destcert.com>
2024-12-31 20:22:38 -0300

    baseurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
-// export const baseurl = 'https://joaobackend.vercel.app/api/';
-export const baseurl = 'http://localhost:3000/api/';
+export const baseurl = 'https://joaobackend.vercel.app/api/';
+// export const baseurl = 'http://localhost:3000/api/';
 
 export const environment = {
   token: 'jwt-token',
